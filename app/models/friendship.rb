5f93a5c8eb63db5280b9651bfa40c598c4f76eec
jubaan <julio.ab@outlook.com>
2020-07-17 15:36:38 +0000

    Fix linter error

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,6 @@
 class Friendship < ApplicationRecord
-  belongs_to :receiver, class_name: "User"
-  belongs_to :requester, class_name: "User"
+  belongs_to :receiver, class_name: 'User'
+  belongs_to :requester, class_name: 'User'
 
   validate :uniqueness_of_mirrored_pairs, on: :create
   validate :prevent_self_association
@@ -12,13 +12,13 @@ class Friendship < ApplicationRecord
 
   def uniqueness_of_mirrored_pairs
     if self.class.where(requester: requester, receiver: receiver)
-      .or(self.class.where(requester: receiver, receiver: requester))
-      .exists?
-      errors.add(:base, "This friendship exists")
+        .or(self.class.where(requester: receiver, receiver: requester))
+        .exists?
+      errors.add(:base, 'This friendship exists')
     end
   end
 
   def prevent_self_association
-    errors.add(:base, "Requester and receiver have to be different") if requester == receiver
+    errors.add(:base, 'Requester and receiver have to be different') if requester == receiver
   end
 end
