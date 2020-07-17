14bf865b01f1b67e701bc290be81cc196a6db4fc
jubaan <julio.ab@outlook.com>
2020-07-17 15:29:16 +0000

    Fix 'Accept Invitation' button

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,6 +1,6 @@
 class Friendship < ApplicationRecord
-  belongs_to :receiver, class_name: 'User'
-  belongs_to :requester, class_name: 'User'
+  belongs_to :receiver, class_name: "User"
+  belongs_to :requester, class_name: "User"
 
   validate :uniqueness_of_mirrored_pairs
   validate :prevent_self_association
@@ -11,14 +11,14 @@ class Friendship < ApplicationRecord
   private
 
   def uniqueness_of_mirrored_pairs
-    if self.class.where(requester: requester, receiver: receiver)
-        .or(self.class.where(requester: receiver, receiver: requester))
-        .exists?
-      errors.add(:base, 'This friendship exists')
+    if self.class.where(requester: requester, receiver: receiver, confirmed: true)
+      .or(self.class.where(requester: receiver, receiver: requester, confirmed: false))
+      .exists?
+      errors.add(:base, "This friendship exists")
     end
   end
 
   def prevent_self_association
-    errors.add(:base, 'Requester and receiver have to be different') if requester == receiver
+    errors.add(:base, "Requester and receiver have to be different") if requester == receiver
   end
 end
