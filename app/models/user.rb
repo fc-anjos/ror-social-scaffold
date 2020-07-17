5b6e7d75c3c024c19831d885c1978cb95f1e7f76
jubaan <julio.ab@outlook.com>
2020-07-17 22:19:58 +0000

    Update omniauth config in user model and devise.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,9 +53,11 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.provider = auth.provider
+      user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name
+      user.name = auth.ifo.name
     end
   end
 end
