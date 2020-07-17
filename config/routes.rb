20287b799a32cca3b20f1d3c40082e255540d916
jubaan <julio.ab@outlook.com>
2020-07-17 22:19:58 +0000

    Update omniauth config in user model and devise.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks_controller" }
+  devise_for :users, controllers: { omniauth_callbacks: "callbacks" }
   root to: "posts#index"
 
   resources :users, only: [:index, :show]
