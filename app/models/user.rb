a76a56481613b0f598dd307782fac9647a5031fd
fc-anjos <felipe.anjos@usp.br>
2020-07-21 17:59:40 -0300

    Merge friendship_v2

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,14 +14,6 @@ class User < ApplicationRecord
   has_many :likes, dependent: :destroy
 
   has_many :requested_friendships,
-<<<<<<< HEAD
-           -> { not_confirmed },
-           class_name: "Friendship", foreign_key: "requester_id"
-
-  has_many :received_friendships,
-           -> { not_confirmed },
-           class_name: "Friendship", foreign_key: "receiver_id"
-=======
            -> { requested },
            class_name: 'Friendship'
 
@@ -32,7 +24,6 @@ class User < ApplicationRecord
   has_many :confirmed_friendships,
            -> { confirmed },
            class_name: 'Friendship'
->>>>>>> friendship_v2
 
   has_many :received_friends,
            class_name: "User",
@@ -44,25 +35,11 @@ class User < ApplicationRecord
            through: :requested_friendships,
            source: :friend
 
-<<<<<<< HEAD
-  has_many :confirmed_friendships,
-           lambda { |object|
-             unscope(where: :user_id).where(
-               "(requester_id = ? OR receiver_id = ?) AND (confirmed =? )",
-               object.id,
-               object.id,
-               true
-             )
-           },
-           class_name: "Friendship"
 
-  def friends
-    User.where(
-      id: confirmed_friendships.pluck(:requester_id) +
-          confirmed_friendships.pluck(:receiver_id),
-    ) -
-      User.where("id =?", id)
-  end
+  has_many :friends,
+           class_name: 'User',
+           through: :confirmed_friendships,
+           source: :friend
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -72,15 +49,10 @@ class User < ApplicationRecord
       user.password = Devise.friendly_token[0, 20]
       user.name = auth.ifo.name
     end
-=======
-  has_many :friends,
-           class_name: 'User',
-           through: :confirmed_friendships,
-           source: :friend
+  end
 
   def timeline_posts
     ids = friends.pluck(:id) << id
     Post.all.ordered_by_most_recent.where(user_id: ids).includes(:user)
->>>>>>> friendship_v2
   end
 end
