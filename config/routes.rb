a2b8bfb557700ea98fda2419e9ab76c4fc5d7128
jubaan <julio.ab@outlook.com>
2020-07-21 22:42:16 +0000

    Update routes with OMNIAUTH_CALLBACK

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: { omniauth_callbacks: "users/callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "users/omniauth_callbacks" }
   root to: "posts#index"
 
   resources :users, only: %i[index show]
