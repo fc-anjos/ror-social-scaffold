b2a22209f29b5fa25f3d7c4509f30030d72fb96f
fc-anjos <felipe.anjos@usp.br>
2020-07-21 18:50:25 -0300

    Add omniauth provider to user model

--- db/schema.rb
+++ db/schema.rb
@@ -60,7 +60,7 @@ ActiveRecord::Schema.define(version: 2020_07_17_202625) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "gravatar_url"
-    t.string "proveder"
+    t.string "provider"
     t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
