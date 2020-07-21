72b0e73ed6236c013a74bc8f1559f6fff3dc4ff7
fc-anjos <felipe.anjos@usp.br>
2020-07-21 18:54:25 -0300

    Change the way provider is passed on user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -26,15 +26,14 @@ class User < ApplicationRecord
            class_name: 'Friendship'
 
   has_many :received_friends,
-           class_name: "User",
+           class_name: 'User',
            through: :received_friendships,
            source: :friend
 
   has_many :requested_friends,
-           class_name: "User",
+           class_name: 'User',
            through: :requested_friendships,
            source: :friend
-
 
   has_many :friends,
            class_name: 'User',
@@ -43,11 +42,9 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.provider = auth.provider
-      user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.info.name
+      user.name = auth.info.name # assuming the user model has a name
     end
   end
 
