57d4967b01b5c0494b580c32c213b956e9092a7b
fc-anjos <felipe.anjos@usp.br>
2020-07-21 18:44:46 -0300

    Add omniauth provider to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,7 +47,7 @@ class User < ApplicationRecord
       user.uid = auth.uid
       user.email = auth.info.email
       user.password = Devise.friendly_token[0, 20]
-      user.name = auth.ifo.name
+      user.name = auth.info.name
     end
   end
 
