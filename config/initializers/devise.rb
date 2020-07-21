a64c2afe496de67fd075dd0570662cc8a24d6b6b
jubaan <julio.ab@outlook.com>
2020-07-21 22:30:17 +0000

    Delete github callback from devise initializer

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -296,5 +296,5 @@ Devise.setup do |config|
   # When set to false, does not sign a user in automatically after their password is
   # changed. Defaults to true, so a user is signed in automatically after changing a password.
   # config.sign_in_after_change_password = true
-  config.omniauth :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], callback_url: "https://calm-coast-20210.herokuapp.com/users/auth/github/callback", scope: "user:email"
+  config.omniauth :github, ENV["GITHUB_KEY"], ENV["GITHUB_SECRET"], scope: "user:email"
 end
