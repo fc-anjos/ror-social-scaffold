b86d0054ad6bf6a2407b05399e6224cd0ea7ed80
jubaan <julio.ab@outlook.com>
2020-07-22 22:01:20 +0000

    add styling to sign up form

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,17 @@ module ApplicationHelper
     link_to link_text, link_path, class: "nav-link"
   end
 
+  def provider_sign_in_btn
+    if devise_mapping.omniauthable?
+      links = []
+      resource_class.omniauth_providers.each do |provider|
+        provider = link_to "Sign in with #{OmniAuth::Utils.camelize(provider)}", omniauth_authorize_path(resource_name, provider), class: "btn btn-primary d-block mb-3"
+        links << provider
+      end
+    end
+    render html: links.join.html_safe
+  end
+
   def like_or_dislike_btn(post)
     like = Like.find_by(post: post, user: current_user)
     if like
