f1a06619df0b8b119495e9342e34ce051bf4af99
jubaan <julio.ab@outlook.com>
2020-07-23 20:27:01 +0000

    Add style to users profile and all user section

--- app/models/user.rb
+++ app/models/user.rb
@@ -13,20 +13,20 @@ class User < ApplicationRecord
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
-  has_many :requested_friendships, -> { requested }, class_name: 'Friendship'
+  has_many :requested_friendships, -> { requested }, class_name: "Friendship"
 
-  has_many :received_friendships, -> { received }, class_name: 'Friendship'
+  has_many :received_friendships, -> { received }, class_name: "Friendship"
 
-  has_many :confirmed_friendships, -> { confirmed }, class_name: 'Friendship'
+  has_many :confirmed_friendships, -> { confirmed }, class_name: "Friendship"
 
   has_many :received_friends,
-           class_name: 'User', through: :received_friendships, source: :friend
+           class_name: "User", through: :received_friendships, source: :friend
 
   has_many :requested_friends,
-           class_name: 'User', through: :requested_friendships, source: :friend
+           class_name: "User", through: :requested_friendships, source: :friend
 
   has_many :friends,
-           class_name: 'User', through: :confirmed_friendships, source: :friend
+           class_name: "User", through: :confirmed_friendships, source: :friend
 
   def self.from_omniauth(auth)
     where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
@@ -39,5 +39,9 @@ class User < ApplicationRecord
   def timeline_posts
     ids = friends.pluck(:id) << id
     Post.all.ordered_by_most_recent.where(user_id: ids).includes(:user)
+  end
+
+  def friendable
+    User.all - (requested_friends + received_friends + friends + [self])
   end
 end
