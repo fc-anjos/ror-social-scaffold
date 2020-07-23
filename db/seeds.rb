2e402e18354d36288b6d485b6428de7b772cdb4d
jubaan <julio.ab@outlook.com>
2020-07-23 20:27:01 +0000

    Add style to users profile and all user section

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,23 +40,23 @@ requested_friend = User.create(
 # Friendship.create({ user: user, friend: requested_friend, status: 'requested' })
 # Friendship.create({ user: user, friend: received_friend, status: 'received' })
 
-# 10.times do
-#   User.create(
-#     {
-#       name: Faker::Name.unique.name,
-#       email: Faker::Internet.unique.email,
-#       password: '123456',
-#       password_confirmation: '123456'
-#     }
-#   )
-# end
+10.times do
+  User.create(
+    {
+      name: Faker::Name.unique.name,
+      email: Faker::Internet.unique.email,
+      password: "123456",
+      password_confirmation: "123456",
+    }
+  )
+end
 
-# User.all.each do |user|
-#   friendable = User.all.ids
-#   5.times do
-#     friend_id = friendable.delete(rand(0..User.all.length))
-#     status = %w[confirmed requested received].sample
-#     Friendship.create({ user_id: user.id, friend_id: friend_id, status: status })
-#     Post.create(user: user, content: Faker::Quotes::Shakespeare.romeo_and_juliet_quote)
-#   end
-# end
+User.all.each do |user|
+  friendable = User.all.ids
+  5.times do
+    friend_id = friendable.delete(rand(0..User.all.length))
+    status = %w[confirmed requested received].sample
+    Friendship.create({ user_id: user.id, friend_id: friend_id, status: status })
+    Post.create(user: user, content: Faker::Quotes::Shakespeare.romeo_and_juliet_quote)
+  end
+end
