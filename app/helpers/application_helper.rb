3368d8e6336a2943a78dfa9c64aa144d32da46de
fc-anjos <felipe.anjos@usp.br>
2020-07-24 19:45:47 -0300

    Correct styling offenses

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,13 +1,15 @@
 module ApplicationHelper
   def menu_link_to(link_text, link_path)
-    link_to link_text, link_path, class: "nav-link"
+    link_to link_text, link_path, class: 'nav-link'
   end
 
   def provider_sign_in_btn
     if devise_mapping.omniauthable?
       links = []
       resource_class.omniauth_providers.each do |provider|
-        provider = link_to "Sign in with #{OmniAuth::Utils.camelize(provider)}", omniauth_authorize_path(resource_name, provider), class: "btn btn-primary d-block mb-3"
+        provider = link_to "Sign in with #{OmniAuth::Utils.camelize(provider)}",
+                           omniauth_authorize_path(resource_name, provider),
+                           class: 'btn btn-primary d-block mb-3'
         links << provider
       end
     end
@@ -17,9 +19,9 @@ module ApplicationHelper
   def like_or_dislike_btn(post)
     like = Like.find_by(post: post, user: current_user)
     if like
-      link_to("Dislike!", post_like_path(id: like.id, post_id: post.id), method: :delete)
+      link_to('Dislike!', post_like_path(id: like.id, post_id: post.id), method: :delete)
     else
-      link_to("Like!", post_likes_path(post_id: post.id), method: :post)
+      link_to('Like!', post_likes_path(post_id: post.id), method: :post)
     end
   end
 end
--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -5,42 +5,42 @@ module UserHelper
     new_friendship_path =
       friendships_path({ user_id: current_user.id, friend_id: user.id })
 
-    class_html = "profile-link"
+    class_html = 'profile-link'
     friendship = find_friendship(user)
 
     if current_user.friends.include?(user)
       render html: link_to(
-        "Unfriend",
+        'Unfriend',
         "/friendships/destroypair/#{friendship.id}",
-        method: :delete, class: "#{class_html} badge badge-secondary",
+        method: :delete, class: "#{class_html} badge badge-secondary"
       )
     elsif current_user.received_friends.include?(user)
       reject_friendship =
         link_to(
-          "Reject",
+          'Reject',
           "/friendships/destroypair/#{friendship.id}",
-          method: :delete, class: "#{class_html} badge badge-danger",
+          method: :delete, class: "#{class_html} badge badge-danger"
         )
 
       accept_friendship =
         link_to(
-          "Accept",
+          'Accept',
           "/friendships/acceptpair/#{friendship.id}",
-          method: :patch, class: "#{class_html} badge badge-success",
+          method: :patch, class: "#{class_html} badge badge-success"
         )
 
       render html: "<span>#{accept_friendship}</span> <span>#{reject_friendship}</span> ".html_safe
     elsif current_user.requested_friends.include?(user)
       render html: link_to(
-        "Drop invitation",
+        'Drop invitation',
         "/friendships/destroypair/#{friendship.id}",
-        method: :delete, class: "#{class_html} badge badge-warning",
+        method: :delete, class: "#{class_html} badge badge-warning"
       )
     else
       render html: link_to(
-        "Invite",
+        'Invite',
         new_friendship_path,
-        method: :post, class: "#{class_html} badge badge-info",
+        method: :post, class: "#{class_html} badge badge-info"
       )
     end
   end
